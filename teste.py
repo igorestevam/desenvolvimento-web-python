b7129d428b635458343749f92a1ebe4bb08ad6b5
Igor Estevam <igorestevam1900@gmail.com>
2026-08-06 09:45:24 -0300

    Update teste.py

--- teste.py
+++ teste.py
@@ -2,3 +2,12 @@ def criar_usuario(**dados):
     print("Novo usuario criado com os dados :", dados)
 
 criar_usuario(nome="Maria", idade=29, cidade="Serra", curso="Computacao")
+
+# ---------------------------------------------
+my_list = ["Ola", 2, 78.5, [1, True, "Casa"]]
+print(my_list[-2])
+my_list[-2] = False
+print(my_list)
+
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+print(matrix)
